e4769a0a90ce61eb551cba271e53f53547a955ff
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-09-02 18:36:44 +0200

    feat: setup express app structure, session and passport middleware

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const prisma = require("./lib/prisma");
+const express = require("express");
+const path = require("node:path");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const passport = require("./config/passport");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
