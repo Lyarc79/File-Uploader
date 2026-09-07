b3d3a29d08fff365e651dd9db350e741d491f867
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-09-07 19:33:59 +0200

    feat(config): configure multer storage middleware

---
+++ config/multer.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const crypto = require("crypto");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(16, function (err, raw) {
+      if (err) return cb(err);
+      cb(null, file.fieldname + "-" + raw.toString("hex"));
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
