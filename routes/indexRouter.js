6b9e0a6bf0da80781638a86fb647b0181b727fcf
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-09-17 18:37:12 +0200

    feat(routes): file delete and download endpoints

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -25,16 +25,18 @@ indexRouter.post("/folders", indexController.postCreateFolder);
 indexRouter.get("/folders/:id", indexController.getFolderDetails);
 indexRouter.post("/folders/:id/update", indexController.postUpdateFolder);
 indexRouter.post("/folders/:id/delete", indexController.postDeleteFolder);
+
 indexRouter.post(
   "/folders/:id/upload",
   upload.single("uploadedFile"),
   indexController.postUploadFileForm,
 );
-
 indexRouter.post(
   "/upload",
   upload.single("uploadedFile"),
   indexController.postUploadFileForm,
 );
+indexRouter.post("/files/:id/delete", indexController.postDeleteFile);
+indexRouter.get("/files/:id/download", indexController.downloadFile);
 
 module.exports = indexRouter;
